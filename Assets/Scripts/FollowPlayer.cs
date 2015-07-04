83012905efe6789acf290e245b6ccb5c6291a468
zhudianyu <1462415060@qq.com>
2015-07-04 22:59:50 +0800

    完成对角色方向的控制

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private Transform player;
+    public float speed = 2;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targePos = new Vector3(0, 3.3f, -2f) + player.position;
+        transform.position = Vector3.Lerp(transform.position, targePos, speed * Time.deltaTime);
+        Quaternion rot = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    private CharacterController cc;
+    public float moveSpeed = 4;
+    void Awake()
+    {
+        cc = this.gameObject.GetComponent<CharacterController>();
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(Mathf.Abs(h)>0.1f|| Mathf.Abs(v)>0.1f)
+        {
+            Vector3 targetPos = new Vector3(h, 0, v);
+            //人物的朝向
+            transform.LookAt(targetPos + transform.position);
+            cc.SimpleMove(transform.forward * moveSpeed);
+        }
+	}
+}
