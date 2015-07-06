6ae210fd3766e949c073ea689725d33c93660a04
zhudianyu <1462415060@qq.com>
2015-07-06 17:02:02 +0800

    完成怪物的自动生成

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public GameObject Spawn()
+    {
+        return GameObject.Instantiate(prefab) as GameObject;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SpawnManager : MonoBehaviour {
+
+    public EnemySpawn[] monsterArray;
+    public EnemySpawn[] bossArray;
+    public List<GameObject> enmeyList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    IEnumerator SpawnEnemy()
+    {
+        foreach (EnemySpawn enemy in monsterArray)
+        {
+            enmeyList.Add( enemy.Spawn());
+        }
+        if (enmeyList.Count > 0)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+
+    }
+}
